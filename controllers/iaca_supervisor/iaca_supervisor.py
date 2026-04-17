6872800b25e7cd141c86c6ebe8c2cffcb286adfe
“Seatea22” <cjt7922@rit.edu>
2026-04-17 13:18:09 -0400

    Controller modules for imports

--- controllers/iaca_supervisor/iaca_supervisor.py
+++ controllers/iaca_supervisor/iaca_supervisor.py
@@ -9,7 +9,7 @@ current_dir = os.path.dirname(__file__)
 shared_path = os.path.abspath(os.path.join(current_dir, "..", "shared"))
 sys.path.append(shared_path)
 
-import equations as eq
+import controllers.shared.equations as eq
 
 
 def clamp(value, value_min, value_max):
