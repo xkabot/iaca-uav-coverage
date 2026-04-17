906fbed0ac67842f309864a4ba8d3fb1d8aabfc2
“Seatea22” <cjt7922@rit.edu>
2026-04-17 13:25:55 -0400

    Added vectorization updates to pheromone local update

--- controllers/iaca_drone/iaca_drone.py
+++ controllers/iaca_drone/iaca_drone.py
@@ -12,7 +12,7 @@ current_dir = os.path.dirname(__file__)
 shared_path = os.path.abspath(os.path.join(current_dir, "..", "shared"))
 sys.path.append(shared_path)
 
-import equations as eq
+import controllers.shared.equations as eq
 
 
 FLYING_ATTITUDE = 2.0
